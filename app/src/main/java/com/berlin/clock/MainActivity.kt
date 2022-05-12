cf0d8bedd51bf34ab232979810300f30cb43fdb9
2022-DEV2-002 <Innovative.apps@yandex.com>
2022-05-13 02:46:05 +0400

    Create CountDownTimer and show the current time every one second

--- app/src/main/java/com/berlin/clock/MainActivity.kt
+++ app/src/main/java/com/berlin/clock/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.berlin.clock
 
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.widget.TextView
+import java.text.SimpleDateFormat
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val tvClock:TextView = findViewById(R.id.tv_text_clock)
+
+        var timer = object: CountDownTimer(Long.MAX_VALUE, 1000){
+            override fun onTick(millisUntilFinished: Long) {
+                val sdf = SimpleDateFormat("hh:mm:ss")
+                val currentDate = sdf.format(Date())
+                tvClock.text = currentDate
+            }
+
+            override fun onFinish() {}
+        }
+        timer.start()
     }
 }
