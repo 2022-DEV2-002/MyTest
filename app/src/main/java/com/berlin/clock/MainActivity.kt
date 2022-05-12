db3fc7678022ce97caa30db6aedb50043767b02c
2022-DEV2-002 <Innovative.apps@yandex.com>
2022-05-13 02:57:27 +0400

    Recreate the time when the time finishes to keep the clock working for ever

--- app/src/main/java/com/berlin/clock/MainActivity.kt
+++ app/src/main/java/com/berlin/clock/MainActivity.kt
@@ -9,20 +9,26 @@ import java.text.SimpleDateFormat
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var timer:CountDownTimer
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val tvClock:TextView = findViewById(R.id.tv_text_clock)
+        timer = createTimer(tvClock)
+        timer.start()
+    }
 
-        var timer = object: CountDownTimer(Long.MAX_VALUE, 1000){
+    private fun createTimer(tvClock:TextView): CountDownTimer {
+        return object: CountDownTimer(Long.MAX_VALUE, 1000){
             override fun onTick(millisUntilFinished: Long) {
                 val sdf = SimpleDateFormat("hh:mm:ss")
                 val currentDate = sdf.format(Date())
                 tvClock.text = currentDate
             }
-
-            override fun onFinish() {}
+            override fun onFinish() {
+                timer = createTimer(tvClock)
+            }
         }
-        timer.start()
     }
 }
