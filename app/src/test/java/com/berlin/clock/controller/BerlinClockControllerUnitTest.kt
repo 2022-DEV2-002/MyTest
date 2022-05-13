0a74993a64380340bdd7a4c03cb852005fe4a59c
2022-DEV2-002 <Innovative.apps@yandex.com>
2022-05-13 08:11:25 +0400

    Test clock illuminate

--- app/src/test/java/com/berlin/clock/controller/BerlinClockControllerUnitTest.kt
+++ app/src/test/java/com/berlin/clock/controller/BerlinClockControllerUnitTest.kt
@@ -23,4 +23,84 @@ class BerlinClockControllerUnitTest{
         val clockExpected = ClockLamps(secondsLamp = LampColor.WHITE)
         assertEquals(clockExpected, clockResult)
     }
+
+    @Test
+    fun `test clock 11_37_12`() {
+        val clockResult = BerlinClockController(13,37,12).getBerlinClockLamps()
+
+        val fiveHoursLamps: Array<LampColor> = Array(4){ _-> LampColor.WHITE}
+        fiveHoursLamps[0]=LampColor.RED
+        fiveHoursLamps[1]=LampColor.RED
+        val oneHourLamps: Array<LampColor> = Array(4){ _-> LampColor.WHITE}
+        oneHourLamps[0]=LampColor.RED
+        oneHourLamps[1]=LampColor.RED
+        oneHourLamps[2]=LampColor.RED
+        val fiveMinutesLamps: Array<LampColor> = Array(11){ _-> LampColor.WHITE}
+        for (i:Int in 0..6)
+            fiveMinutesLamps[i] = LampColor.YELLOW
+
+            fiveMinutesLamps[2] = LampColor.RED
+            fiveMinutesLamps[5] = LampColor.RED
+        val oneMinutesLamps: Array<LampColor> = Array(4){ _-> LampColor.WHITE}
+        oneMinutesLamps[0]=LampColor.YELLOW
+        oneMinutesLamps[1]=LampColor.YELLOW
+
+        val clockExpected = ClockLamps(
+            secondsLamp = LampColor.RED,
+            fiveHoursLamps = fiveHoursLamps,
+            oneHourLamps = oneHourLamps,
+            fiveMinutesLamps = fiveMinutesLamps,
+            oneMinutesLamps = oneMinutesLamps
+        )
+        assertEquals(clockExpected, clockResult)
+    }
+
+    @Test
+    fun `test clock 06_59_36`() {
+        val clockResult = BerlinClockController(6,59,36).getBerlinClockLamps()
+
+        val fiveHoursLamps: Array<LampColor> = Array(4){ _-> LampColor.WHITE}
+        fiveHoursLamps[0]=LampColor.RED
+        val oneHourLamps: Array<LampColor> = Array(4){ _-> LampColor.WHITE}
+        oneHourLamps[0]=LampColor.RED
+        val fiveMinutesLamps: Array<LampColor> = Array(11){ _-> LampColor.YELLOW}
+        fiveMinutesLamps[2] = LampColor.RED
+        fiveMinutesLamps[5] = LampColor.RED
+        fiveMinutesLamps[8] = LampColor.RED
+        val oneMinutesLamps: Array<LampColor> = Array(4){ _-> LampColor.YELLOW}
+
+        val clockExpected = ClockLamps(
+            secondsLamp = LampColor.RED,
+            fiveHoursLamps = fiveHoursLamps,
+            oneHourLamps = oneHourLamps,
+            fiveMinutesLamps = fiveMinutesLamps,
+            oneMinutesLamps = oneMinutesLamps
+        )
+        assertEquals(clockExpected, clockResult)
+    }
+
+    @Test
+    fun `test clock 22_08_41`() {
+        val clockResult = BerlinClockController(22,8,41).getBerlinClockLamps()
+
+        val fiveHoursLamps: Array<LampColor> = Array(4){ _-> LampColor.RED}
+        val oneHourLamps: Array<LampColor> = Array(4){ _-> LampColor.WHITE}
+        oneHourLamps[0]=LampColor.RED
+        oneHourLamps[1]=LampColor.RED
+        val fiveMinutesLamps: Array<LampColor> = Array(11){ _-> LampColor.WHITE}
+        fiveMinutesLamps[0] = LampColor.YELLOW
+        val oneMinutesLamps: Array<LampColor> = Array(4){ _-> LampColor.WHITE}
+        oneMinutesLamps[0]=LampColor.YELLOW
+        oneMinutesLamps[1]=LampColor.YELLOW
+        oneMinutesLamps[2]=LampColor.YELLOW
+
+        val clockExpected = ClockLamps(
+            secondsLamp = LampColor.WHITE,
+            fiveHoursLamps = fiveHoursLamps,
+            oneHourLamps = oneHourLamps,
+            fiveMinutesLamps = fiveMinutesLamps,
+            oneMinutesLamps = oneMinutesLamps
+        )
+        assertEquals(clockExpected, clockResult)
+    }
 }
