214a730c72ec7333b14c09e035f89b25d7d2e4bc
2022-DEV2-002 <Innovative.apps@yandex.com>
2022-05-13 07:12:30 +0400

    View minutes

--- app/src/main/java/com/berlin/clock/view/BerlinClockViewer.kt
+++ app/src/main/java/com/berlin/clock/view/BerlinClockViewer.kt
@@ -6,22 +6,83 @@ import com.berlin.clock.R
 import com.berlin.clock.model.ClockLamps
 import com.berlin.clock.model.LampColor
 
-object BerlinClockViewer{
+object BerlinClockViewer {
 
-    fun viewClock(clockLamps: ClockLamps,context:Context,parentView:View){
-        viewSecondsLamp(clockLamps.secondsLamp,context,parentView)
+    fun viewClock(clockLamps: ClockLamps, context: Context, parentView: View) {
+        viewSecondsLamp(clockLamps.secondsLamp, context, parentView)
+        viewOneMinuteLamps(clockLamps.oneMinutesLamps, context, parentView)
+        viewFiveMinutesLamps(clockLamps.fiveMinutesLamps, context, parentView)
     }
 
-    private fun viewFiveHoursLamps(){}
-    private fun viewOneHourLamps(){}
-    private fun viewFiveMinutesLamps(){}
-    private fun viewOneMinuteLamps(){}
-    private fun viewSecondsLamp(secondsLampColor:LampColor,context:Context,parentView:View){
-        //val lampId= view.resources.getIdentifier("vi_seconds_lamp", "id", context.packageName)
-        val lamp:View = parentView.findViewById(R.id.vi_seconds_lamp)
-        if(secondsLampColor == LampColor.WHITE)
+    private fun viewFiveHoursLamps() {}
+    private fun viewOneHourLamps() {}
+    private fun viewFiveMinutesLamps(
+        fiveMinutesLamps: Array<LampColor>,
+        context: Context,
+        parentView: View
+    ) {
+        val fiveMinutesLampTemplateID = "vi_five_minutes_lamp_"
+        illuminateLamps(fiveMinutesLampTemplateID, fiveMinutesLamps, context, parentView)
+    }
+
+    private fun viewOneMinuteLamps(
+        oneMinutesLamps: Array<LampColor>,
+        context: Context,
+        parentView: View
+    ) {
+        val oneMinuteLampTemplateID = "vi_one_minute_lamp_"
+        illuminateLamps(oneMinuteLampTemplateID, oneMinutesLamps, context, parentView)
+    }
+
+    private fun viewSecondsLamp(secondsLampColor: LampColor, context: Context, parentView: View) {
+        val lamp: View = parentView.findViewById(R.id.vi_seconds_lamp)
+        if (secondsLampColor == LampColor.WHITE)
             lamp.background = context.getDrawable(R.drawable.circle_white)
-            else
+        else
             lamp.background = context.getDrawable(R.drawable.circle_red)
+    }
+
+    private fun illuminateLamps(
+        fiveMinutesLampTemplateID: String,
+        lamps: Array<LampColor>,
+        context: Context,
+        parentView: View
+    ) {
+        val lastIndex = lamps.size-1
+        for (index: Int in 0..lastIndex) {
+            val lamp = getLamp(fiveMinutesLampTemplateID + index, context, parentView)
+            when (lamps[index]) {
+                LampColor.WHITE -> {
+                    when (index) {
+                        0 -> lamp.background = context.getDrawable(R.drawable.left_rectangle_white)
+                        lastIndex -> lamp.background =
+                            context.getDrawable(R.drawable.right_rectangle_white)
+                        else -> lamp.background = context.getDrawable(R.drawable.rectangle_white)
+                    }
+                }
+                LampColor.YELLOW -> {
+                    when (index) {
+                        0 -> lamp.background = context.getDrawable(R.drawable.left_rectangle_yellow)
+                        lastIndex -> lamp.background =
+                            context.getDrawable(R.drawable.right_rectangle_yello)
+                        else -> lamp.background = context.getDrawable(R.drawable.rectangle_yellow)
+                    }
+                }
+                LampColor.RED -> {
+                    when (index) {
+                        0 -> lamp.background = context.getDrawable(R.drawable.left_rectangle_red)
+                        lastIndex -> lamp.background =
+                            context.getDrawable(R.drawable.right_rectangle_red)
+                        else -> lamp.background = context.getDrawable(R.drawable.rectangle_red)
+                    }
+                }
+            }
+        }
+    }
+
+    private fun getLamp(id: String, context: Context, parentView: View): View {
+        val lampId =
+            parentView.resources.getIdentifier(id, "id", context.packageName)
+        return parentView.findViewById(lampId)
     }
 }
