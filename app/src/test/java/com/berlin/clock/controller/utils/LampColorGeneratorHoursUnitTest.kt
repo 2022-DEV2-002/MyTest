2fa250bb926a949c0a4e34e526b344ca95238ad1
2022-DEV2-002 <Innovative.apps@yandex.com>
2022-05-13 07:39:08 +0400

    Enhance hours tests

--- app/src/test/java/com/berlin/clock/controller/utils/LampColorGeneratorHoursUnitTest.kt
+++ app/src/test/java/com/berlin/clock/controller/utils/LampColorGeneratorHoursUnitTest.kt
@@ -12,35 +12,35 @@ class LampColorGeneratorHoursUnitTest {
     @Test
     fun `test all hours are white`() {
         val hours = 0
-        val fiveMinutesLampsResult = LampColorGenerator.generateFiveHoursLamps(hours)
-        val oneMinuteLampsResult = LampColorGenerator.generateOneHourLamps(hours)
+        val fiveHoursLampsResult = LampColorGenerator.generateFiveHoursLamps(hours)
+        val oneHourLampsResult = LampColorGenerator.generateOneHourLamps(hours)
 
         val fiveHoursLampsExpected = Array(4){ _-> LampColor.WHITE}
         val oneHourLampsExpected = Array(4){ _-> LampColor.WHITE}
 
-        Assert.assertArrayEquals(fiveHoursLampsExpected, fiveMinutesLampsResult)
-        Assert.assertArrayEquals(oneHourLampsExpected, oneMinuteLampsResult)
+        Assert.assertArrayEquals(fiveHoursLampsExpected, fiveHoursLampsResult)
+        Assert.assertArrayEquals(oneHourLampsExpected, oneHourLampsResult)
     }
 
     @Test
     fun `test 01 hours`() {
         val hours = 1
-        val fiveMinutesLampsResult = LampColorGenerator.generateFiveHoursLamps(hours)
-        val oneMinuteLampsResult = LampColorGenerator.generateOneHourLamps(hours)
+        val fiveHoursLampsResult = LampColorGenerator.generateFiveHoursLamps(hours)
+        val oneHourLampsResult = LampColorGenerator.generateOneHourLamps(hours)
 
         val fiveHoursLampsExpected = Array(4){ _-> LampColor.WHITE}
         val oneHourLampsExpected = Array(4){ _-> LampColor.WHITE}
         oneHourLampsExpected[0] = LampColor.RED
 
-        Assert.assertArrayEquals(fiveHoursLampsExpected, fiveMinutesLampsResult)
-        Assert.assertArrayEquals(oneHourLampsExpected, oneMinuteLampsResult)
+        Assert.assertArrayEquals(fiveHoursLampsExpected, fiveHoursLampsResult)
+        Assert.assertArrayEquals(oneHourLampsExpected, oneHourLampsResult)
     }
 
     @Test
     fun `test 11 hours`() {
         val hours = 11
-        val fiveMinutesLampsResult = LampColorGenerator.generateFiveHoursLamps(hours)
-        val oneMinuteLampsResult = LampColorGenerator.generateOneHourLamps(hours)
+        val fiveHoursLampsResult = LampColorGenerator.generateFiveHoursLamps(hours)
+        val oneHourLampsResult = LampColorGenerator.generateOneHourLamps(hours)
 
         val fiveHoursLampsExpected = Array(4){ _-> LampColor.WHITE}
         fiveHoursLampsExpected[0] = LampColor.RED
@@ -49,15 +49,15 @@ class LampColorGeneratorHoursUnitTest {
         val oneHourLampsExpected = Array(4){ _-> LampColor.WHITE}
         oneHourLampsExpected[0] = LampColor.RED
 
-        Assert.assertArrayEquals(fiveHoursLampsExpected, fiveMinutesLampsResult)
-        Assert.assertArrayEquals(oneHourLampsExpected, oneMinuteLampsResult)
+        Assert.assertArrayEquals(fiveHoursLampsExpected, fiveHoursLampsResult)
+        Assert.assertArrayEquals(oneHourLampsExpected, oneHourLampsResult)
     }
 
     @Test
     fun `test 17 hours`() {
         val hours = 17
-        val fiveMinutesLampsResult = LampColorGenerator.generateFiveHoursLamps(hours)
-        val oneMinuteLampsResult = LampColorGenerator.generateOneHourLamps(hours)
+        val fiveHoursLampsResult = LampColorGenerator.generateFiveHoursLamps(hours)
+        val oneHourLampsResult = LampColorGenerator.generateOneHourLamps(hours)
 
         val fiveHoursLampsExpected = Array(4){ _-> LampColor.WHITE}
         fiveHoursLampsExpected[0] = LampColor.RED
@@ -68,30 +68,30 @@ class LampColorGeneratorHoursUnitTest {
         oneHourLampsExpected[0] = LampColor.RED
         oneHourLampsExpected[1] = LampColor.RED
 
-        Assert.assertArrayEquals(fiveHoursLampsExpected, fiveMinutesLampsResult)
-        Assert.assertArrayEquals(oneHourLampsExpected, oneMinuteLampsResult)
+        Assert.assertArrayEquals(fiveHoursLampsExpected, fiveHoursLampsResult)
+        Assert.assertArrayEquals(oneHourLampsExpected, oneHourLampsResult)
     }
 
     @Test
     fun `test 23 hours`() {
         val hours = 23
-        val fiveMinutesLampsResult = LampColorGenerator.generateFiveHoursLamps(hours)
-        val oneMinuteLampsResult = LampColorGenerator.generateOneHourLamps(hours)
+        val fiveHoursLampsResult = LampColorGenerator.generateFiveHoursLamps(hours)
+        val oneHourLampsResult = LampColorGenerator.generateOneHourLamps(hours)
 
         val fiveHoursLampsExpected = Array(4){ _-> LampColor.RED}
 
         val oneHourLampsExpected = Array(4){ _-> LampColor.RED}
         oneHourLampsExpected[3] = LampColor.WHITE
 
-        Assert.assertArrayEquals(fiveHoursLampsExpected, fiveMinutesLampsResult)
-        Assert.assertArrayEquals(oneHourLampsExpected, oneMinuteLampsResult)
+        Assert.assertArrayEquals(fiveHoursLampsExpected, fiveHoursLampsResult)
+        Assert.assertArrayEquals(oneHourLampsExpected, oneHourLampsResult)
     }
 
     @Test
     fun `test 14 hours`() {
         val hours = 14
-        val fiveMinutesLampsResult = LampColorGenerator.generateFiveHoursLamps(hours)
-        val oneMinuteLampsResult = LampColorGenerator.generateOneHourLamps(hours)
+        val fiveHoursLampsResult = LampColorGenerator.generateFiveHoursLamps(hours)
+        val oneHourLampsResult = LampColorGenerator.generateOneHourLamps(hours)
 
         val fiveHoursLampsExpected = Array(4){ _-> LampColor.WHITE}
         fiveHoursLampsExpected[0] = LampColor.RED
@@ -99,21 +99,21 @@ class LampColorGeneratorHoursUnitTest {
 
         val oneHourLampsExpected = Array(4){ _-> LampColor.RED}
 
-        Assert.assertArrayEquals(fiveHoursLampsExpected, fiveMinutesLampsResult)
-        Assert.assertArrayEquals(oneHourLampsExpected, oneMinuteLampsResult)
+        Assert.assertArrayEquals(fiveHoursLampsExpected, fiveHoursLampsResult)
+        Assert.assertArrayEquals(oneHourLampsExpected, oneHourLampsResult)
     }
 
     @Test
     fun `test 9 hours`() {
         val hours = 9
-        val fiveMinutesLampsResult = LampColorGenerator.generateFiveHoursLamps(hours)
-        val oneMinuteLampsResult = LampColorGenerator.generateOneHourLamps(hours)
+        val fiveHoursLampsResult = LampColorGenerator.generateFiveHoursLamps(hours)
+        val oneHourLampsResult = LampColorGenerator.generateOneHourLamps(hours)
 
         val fiveHoursLampsExpected = Array(4){ _-> LampColor.WHITE}
         fiveHoursLampsExpected[0] = LampColor.RED
         val oneHourLampsExpected = Array(4){ _-> LampColor.RED}
 
-        Assert.assertArrayEquals(fiveHoursLampsExpected, fiveMinutesLampsResult)
-        Assert.assertArrayEquals(oneHourLampsExpected, oneMinuteLampsResult)
+        Assert.assertArrayEquals(fiveHoursLampsExpected, fiveHoursLampsResult)
+        Assert.assertArrayEquals(oneHourLampsExpected, oneHourLampsResult)
     }
 }
